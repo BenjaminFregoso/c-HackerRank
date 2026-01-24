feccec838b17239f2460eec5690911508cae2f09
Benjamin Fregoso <ing_benjamin_fre@outlook.com>
2026-01-24 14:28:28 -0600

    Given an array of integers, find the sum of its elements.

---
+++ SumArrayItems/SumArrayItems/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+
+        int lenghtar = ar.Count - 1;
+        int total = 0;
+        while (lenghtar >= 0)
+        {
+            total += ar[lenghtar];
+            lenghtar--;
+        }
+        return total;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> ar = [1, 45, 1, 343, 56, 767, 88];
+
+        int result = Result.simpleArraySum(ar);
+
+        Console.WriteLine(result);
+
+    }
+}
