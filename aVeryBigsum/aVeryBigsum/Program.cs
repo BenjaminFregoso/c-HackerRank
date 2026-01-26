1447354524b1a7f60bf7f0b9a0e31256251427df
Benjamin Fregoso <ing_benjamin_fre@outlook.com>
2026-01-26 13:28:25 -0600

    aVeryBigSum
    In this challenge, you need to calculate and print the sum of elements in an array, considering that some integers may be very large.

---
+++ aVeryBigsum/aVeryBigsum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        int arraySize = ar.Count - 1;
+        long arraySum = 0;
+        while (arraySize >= 0)
+        {
+            arraySum += ar[arraySize];
+            arraySize--;
+        }
+
+        return arraySum;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<long> ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+
+        long result = Result.aVeryBigSum(ar);
+
+        Console.WriteLine(result);
+
+    }
+}
