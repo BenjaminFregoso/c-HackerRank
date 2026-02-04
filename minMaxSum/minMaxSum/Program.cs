6796932af4086ada1a555d72fd03a44f9e7654f0
Benjamin Fregoso <ing_benjamin_fre@outlook.com>
2026-02-04 14:03:31 -0600

    minMaxSum
    Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.

---
+++ minMaxSum/minMaxSum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long minNum = 0;
+        long maxNum = 0;
+        int arraySize = arr.Count - 1;
+
+        arr.Sort();
+
+        while (arraySize >= 0)
+        {
+            if (arraySize == arr.Count - 1)
+            {
+                maxNum += arr[arraySize];
+            }
+            else if (arraySize == 0)
+            {
+                minNum += arr[arraySize];
+            }
+            else
+            {
+                minNum += arr[arraySize];
+                maxNum += arr[arraySize];
+            }
+            arraySize--;
+        }
+        Console.Write(minNum.ToString() + " " + maxNum.ToString());
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = [7, 69, 2, 221, 8974];
+
+        Result.miniMaxSum(arr);
+    }
+}
