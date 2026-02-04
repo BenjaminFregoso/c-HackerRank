de1146d324053a13ab3d54cb6816474e65e00b5d
Benjamin Fregoso <ing_benjamin_fre@outlook.com>
2026-02-04 14:14:06 -0600

    birthdayCakeCandles
    You are in charge of the cake for a child's birthday. It will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Your task is to count how many candles are the tallest.

---
+++ birthdayCakeCandles/birthdayCakeCandles/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int listSize = candles.Count - 1;
+        int tallest = 0;
+        int numTallest = 0;
+
+        while (listSize >= 0)
+        {
+            if (candles[listSize] > tallest)
+            {
+                tallest = candles[listSize];
+                numTallest = 1;
+            }
+            else if (candles[listSize] == tallest)
+            {
+                numTallest++;
+            }
+            listSize--;
+        }
+
+        return numTallest;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> candles = [4, 4, 1, 3];
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        Console.WriteLine(result);
+    }
+}
